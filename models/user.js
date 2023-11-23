887d9f943a403000c0d8b11a3ad04db19044b950
Lujain92 <lujain.aljarah@cds-global.com>
2023-11-23 12:51:34 +0300

    SQL with ORM done

--- models/user.js
+++ models/user.js
@@ -2,26 +2,19 @@ const Sequelize = require('sequelize');
 
 const sequelize = require('../util/database');
 
-const Product = sequelize.define('product', {
+const User = sequelize.define('user', {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true,
   },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false,
-  },
-  imgUrl: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  description: {
+  name: Sequelize.STRING,
+
+  email: {
     type: Sequelize.STRING,
     allowNull: false,
   },
 });
 
-module.exports = Product;
+module.exports = User;
